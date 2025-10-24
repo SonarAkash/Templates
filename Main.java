a3b56b3719fb02b32597b52cce046a22cecb8d12
Akash <akashsonar.9113@gmail.com>
2025-10-25 01:11:32 +0530

    setup before filtering

--- Main.java
+++ Main.java
@@ -5,12 +5,15 @@ import DataStructure.CustomHashMap;
 
 public class Main {
     public static void main(String[] args) {
-      CustomHashMap<Integer, String> mp = new CustomHashMap<>();
-      for(int i=0; i<5; i++){
-        mp.put(i, "Present");
-      }
-      for(Map.Entry<Integer, String> it : mp){
-        System.out.println(it.getKey() + "->" + it.getValue());
-      }
+      // CustomHashMap<Integer, String> mp = new CustomHashMap<>();
+      // for(int i=0; i<5; i++){
+      //   mp.put(i, "Present");
+      // }
+      // for(Map.Entry<Integer, String> it : mp){
+      //   System.out.println(it.getKey() + "->" + it.getValue());
+      // }
+
+
+      for(int i=0; i<args.length; i++) System.out.println(args[i]);
     }
 }
